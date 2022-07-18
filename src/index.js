4e7382a70f4aab38ef1f89a1f3c719e2104aa160
efoosag <96612103+efoosag@users.noreply.github.com>
2022-07-18 11:21:11 +0100

    install redux

--- src/index.js
+++ src/index.js
@@ -1,18 +1,22 @@
+/* eslint-disable comma-dangle */
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import Provider from 'react-redux/es/components/Provider';
+import store from './redux/configureStore';
 import App from './App';
-import './index.css';
 
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>,
+  <Provider store={store}>
+    <React.StrictMode>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </React.StrictMode>
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
